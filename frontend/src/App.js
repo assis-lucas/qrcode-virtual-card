8c5453ce47035e520caa8b931cb1d10dd9df8913
Lucas <lucasassistomaz@gmail.com>
2022-10-31 07:49:49 -0300

    Moving styles into a directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
-import "./index.css";
+import "./styles/index.css";
 
 export default function App() {
   return <RouterProvider router={routes} />;
